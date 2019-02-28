05ddac0f3afcf64b588f62d645afba42662d249c
Константин Викторович Калинин <kkv1210190@gmail.com>
2019-02-28 20:24:36 +0200

    create+table

--- HW1/script.js
+++ HW1/script.js
@@ -1,14 +1,18 @@
-
-document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
-	var colums = document.getElementById('colsAmount');
+document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready
+	var colums = document.getElementById('colsAmount'); //
 	var	rows = document.getElementById('rowsAmount');
 	var createButton = document.getElementById('createNewTable');
 	var tableBody = document.getElementById('dinamic-table-body');
+	var tooltip =document.getElementById('tooltip-elem');
+	
+	/**
+	* Create Table with entered amount of rows and columns
+	*/
 	createButton.addEventListener('click', function(e) {
 		e.preventDefault();
 		tableBody.innerHTML = "";
-		var colsAmount = colums.value;
-		var rowsAmount = rows.value; 
+		var colsAmount = (+colums.value > 100) ? 100 : colums.value;
+		var rowsAmount = (+rows.value> 100) ? 100 : rows.value; 
 		for (var i = 0; i < rowsAmount; i++) {
 			var newTr = document.createElement("TR");
 			for (var j = 0; j< colsAmount; j++){
@@ -18,6 +22,39 @@ document.addEventListener('DOMContentLoaded', function(){ // Аналог $(docu
 			tableBody.appendChild(newTr);
 
 		}
+	});
 
+	/**
+	* Show indexes of derived <td>-element
+	* param (tdElem) - devired td-element
+	*/
+	function getCellCoords(tdElem){
+		if(tdElem.tagName == 'TD'){
+			var rowIndex = tdElem.parentNode.rowIndex+1;
+			var cellIndex = tdElem.cellIndex+1;
+			tooltip.style.top = tdElem.getBoundingClientRect().bottom +'px';
+			tooltip.style.left = tdElem.getBoundingClientRect().left +'px';
+			tooltip.style.opacity = 1;
+			tooltip.style.visibility = 'visible';
+			tooltip.innerHTML = "<div><span>Cell`s coords in table is <b>"+ rowIndex +"</b>(row):<b>"+ cellIndex +"</b>(cell) </span></div>"
+			setTimeout(function(){
+				tooltip.style.opacity = 0;
+				tooltip.style.visibility = 'hidden';	
+			},2000)
+		}
+		return;
+	}
+
+	tableBody.addEventListener('click', function(e){
+		console.log(e.target);
+		var target = e.target;
+	
+		while (target != tableBody) {
+			if (target.tagName == 'TD') {
+				getCellCoords(target);
+				return;
+			}
+			target = target.parentNode;
+		}
 	});
 });
